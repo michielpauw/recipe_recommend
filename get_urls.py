e9048bb1dd3462482e11be14cc760bc8bc35e7f7
Michiel Pauw <pauw.michiel@gmail.com>
2017-09-04 14:59:59 +0200

    Created URL-list

---
+++ get_urls.py
@@ -0,0 +1,16 @@
+import urllib.request
+import re
+from bs4 import BeautifulSoup
+
+url = "https://www.ah.nl/allerhande/recepten-zoeken/__/N-26vq?Nrpp=9288"
+page = urllib.request.urlopen(url)
+file = open('url_list', 'w')
+
+soup = BeautifulSoup(page, 'html.parser')
+cur = 0
+
+for link in soup.find_all('a'):
+    url_str = str(link.get('href'))
+    if (bool(re.search('R-R', url_str)) and cur % 2 == 0):
+        file.write(url_str + '\n')
+    cur += 1
