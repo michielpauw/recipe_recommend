f96af7637ffc8def72b8ee68fc87734703abe8dd
JTJdroid <jessen.thijmen@gmail.com>
2017-09-05 15:36:16 +0200

    database is clean

--- test_environment.py
+++ test_environment.py
@@ -1,7 +1,41 @@
 import pickle
+import random
+import math
 
 with open('recipe_db.pkl', 'rb') as f:
-    mynewlist = pickle.load(f)
+    recipe_db = pickle.load(f)
 
-for i in mynewlist:
-    print(i)
+
+
+# times= []
+# for i in recipe_db:
+#     times.append(i['time'])
+#
+#
+# print((sorted(times, reverse=True)))
+
+# delete items with 0 calories
+# count = 0
+# _recipe_db = []
+# for i in range(len(recipe_db)):
+#     if recipe_db[i]['calories'] != 0:
+#         _recipe_db.append(recipe_db[i])
+# recipe_db = _recipe_db
+#
+# with open('recipe_db.pkl', 'wb') as f:
+#     pickle.dump(recipe_db, f)
+
+# for i in range(len(recipe_db)):
+#     for j in range(len(recipe_db[i]['ingredients'])):
+#         recipe_db[i]['ingredients'][j] = recipe_db[i]['ingredients'][j]['description']
+#
+# print(len(recipe_db))
+# print(recipe_db[6]['ingredients'])
+#
+# with open('recipe_db.pkl', 'wb') as f:
+#     pickle.dump(recipe_db, f)
+
+# ingredients
+# qualities
+# calories check
+# tags check
