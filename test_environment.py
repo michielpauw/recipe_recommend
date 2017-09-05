5b794735c8d5f9550da1d2c3a0b9fc0b388240f2
JTJdroid <jessen.thijmen@gmail.com>
2017-09-05 13:09:35 +0200

    pickel

--- test_environment.py
+++ test_environment.py
@@ -1,17 +1,7 @@
-def del_characters(str1):
-    delims = ["&", "#", '&', '\n', '\t']
-    for delim in delims:
-        print(delim)
-        for i in range(1):
-            try:
-                s = str1.index(delim)
-                print(s)
-                str1 = str1[0:s] + str1[s+1:]
-            except ValueError:
-                pass
-    return str1
+import pickle
 
+with open('recipe_db.pkl', 'rb') as f:
+    mynewlist = pickle.load(f)
 
-new_string = "h&e man,# hoe\n is &het met jou?"
-
-print(del_characters(new_string))
+for i in mynewlist:
+    print(i)
--- transcribe_html.py
+++ transcribe_html.py
@@ -1,18 +1,46 @@
-file_name = "recipes_html/recipe"
+import pickle
 
+file_name = "recipes_html/recipe"
+urls = open('url_list')
+urls = urls.read()
+urls = urls.splitlines()
+
+count = 0
+
+recipes = []
 # loop over all recipes and select the ingredients
-for num in range(3):  # 999
+for num in range(9000):  # 999
+
+    print(num)
 
     recipe_data = dict()
+    recipe_data['link'] = urls[num]
 
     # open and read in the html file
     file = open(file_name + str(num) + ".html")
     html = file.read()
 
+
+    def find_occ(_html_list, count):
+        for i in range(len(_html_list)):
+            if isinstance(_html_list[i], str):
+                if 'div id="ingredients_to_shoplist"' in _html_list[i]:
+                    return i, count
+                elif 'recipeproducts?noNav=true' in html_list[i]:
+                    return i, count
+            elif isinstance(_html_list[i][0], str):
+                if 'div id="ingredients_to_shoplist"' in _html_list[i][0]:
+                    return i, count
+                elif 'recipeproducts?noNav=true' in html_list[i][0]:
+                    return i, count
+            else:
+                return -1, count
+
+
     # split in lines and focus on ingredients
     html_list = html.splitlines()
     x_0 = [i for i in range(len(html_list)) if "list shopping ingredient-selector-list" in html_list[i]][0]
-    x_n = [i for i in range(len(html_list)) if 'div id="ingredients_to_shoplist"' in html_list[i]][0]
+    x_n, count = find_occ(html_list, count)
     html_ingredients = html_list[x_0:x_n]
 
     # get all ingredient classes
@@ -63,31 +91,41 @@ for num in range(3):  # 999
         if "bereidingstijd" in i:
             recipe_data['time'] = int(i.split(":")[1][1:-2])
         if "calorien" in i:
-            recipe_data['calories'] = int(float(i.split(":")[1][1:-2]))
+            try:
+                recipe_data['calories'] = int(float(i.split(":")[1][1:-2]))
+            except ValueError:
+                recipe_data['calories'] = 0
 
     final_tags = []
     for i in range(len(details[:details_end])):
         if "tags" in details[:details_end][i]:
-            tags = details[:details_end][i+2:-1]
+            tags = details[:details_end][i + 2:-1]
             for j in (tags):
-                final_tags.append(''.join(j.split()))
+                tt = ''.join(j.split())[1:-1]
+                if tt[-1] == '"':
+                    tt = tt[:-1]
+                final_tags.append(tt)
 
     recipe_data['tags'] = final_tags
-    print(recipe_data)
+    recipes.append(recipe_data)
 
-            # < script >
-            # window.dataLayer = {
-            #     "page": {
-            #         "type": "recept",
-            #         "context": {
-            #             "titel": "Vegan carrot hotdog",
-            #             "gang": "hoofdgerecht",
-            #             "personen": "8",
-            #             "bereidingstijd": "1460",
-            #             "calorien": "230.0",
-            #             "tags": [
-            #
-            #                 "lactosevrij",
-            #                 "veganistisch",
-            #                 "vegetarisch"
-            #             ],
+with open('recipe_db.pkl', 'wb') as f:
+    pickle.dump(recipes, f)
+
+
+# < script >
+# window.dataLayer = {
+#     "page": {
+#         "type": "recept",
+#         "context": {
+#             "titel": "Vegan carrot hotdog",
+#             "gang": "hoofdgerecht",
+#             "personen": "8",
+#             "bereidingstijd": "1460",
+#             "calorien": "230.0",
+#             "tags": [
+#
+#                 "lactosevrij",
+#                 "veganistisch",
+#                 "vegetarisch"
+#             ],
