eaab203c6185ccbf8e2bda20a89cabc3ba01303a
Michiel Pauw <pauw.michiel@gmail.com>
2017-09-06 16:44:47 +0200

    ghjfgh

--- connect_db.py
+++ connect_db.py
@@ -1,5 +1,21 @@
 from pymongo import MongoClient
 client = MongoClient()
+import pickle
 
 db = client.recipe_recommend
+
+# create collections
 recipes = db.recipes
+users = db.users
+
+with open('recipe_db.pkl', 'rb') as f:
+    mynewlist = pickle.load(f)
+
+for test in mynewlist[0:10]:
+    recipes.insert_one(test)
+
+user = {"first_name": "Michiel",
+        "last_name": "Pauw",
+        "email": "pauw.michiel@gmail.com",
+        "recipes_rec": set(),
+        "recipes_made": dict()}
--- test_environment.py
+++ test_environment.py
@@ -3,5 +3,7 @@ import pickle
 with open('recipe_db.pkl', 'rb') as f:
     mynewlist = pickle.load(f)
 
-for i in mynewlist:
-    print(i)
+testlist = mynewlist[1000:4000:100]
+
+for test in mynewlist[0:10]:
+    print(test)
